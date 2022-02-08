0cf89fbc1635e0c9fbf60f1baa9dc978252ed5c7
ufoo68 <mitarou.yuutarou@gmail.com>
2022-02-08 18:55:49 +0900

    ハンズオン分の削除

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import { Ticket } from './ui-components';
 import { useEffect, useState } from 'react';
-import { Ticket as TicketModel } from './models';
-import { DataStore } from 'aws-amplify';
-import liff from '@line/liff';
 import './App.css';
 
 function App() {
   const [ticket, setTicket] = useState({ user_name: '', user_qr: '' });
   useEffect(() => {
-    DataStore.query(TicketModel).then(async (tickets) => {
-      liff.init({ liffId: process.env.REACT_APP_LIFF_ID }).then(async () => {
-        if (!liff.isLoggedIn()) {
-          await liff.login();
-        }
-        const { userId } = await liff.getProfile();
-        const ticket = tickets.find((t) => t.liff_user_id === userId);
-        if (ticket) {
-          setTicket({ user_name: ticket.user_name, user_qr: ticket.user_qr });
-        }
-      });
-    });
+    // TODO: チケット情報の取得
   });
   const registerTicket = () => {
-    liff.init({ liffId: process.env.REACT_APP_LIFF_ID }).then(async () => {
-      if (!liff.isLoggedIn()) {
-        await liff.login();
-      }
-      const { userId, displayName } = await liff.getProfile();
-      await DataStore.save(
-        new TicketModel({
-          liff_user_id: userId,
-          user_name: displayName,
-        })
-      );
-      const tickets = await DataStore.query(TicketModel);
-      const { user_name, user_qr } = tickets.find((t) => t.liff_user_id === userId);
-      setTicket({ user_name, user_qr });
-    });
+    //TODO:  チケット登録の実装
   }
   return (
     <div className='container'>
+      {/* TODO: チケットの表示 */}
       {ticket.user_name && ticket.user_qr ?
-        <Ticket ticketProp={ticket} /> : ticket.user_name
+        'ここにチケットが表示される' : ticket.user_name
           ? 'チケットが登録されました。チケット発行までしばらくお待ち下さい。' :
           <button onClick={registerTicket}>チケット登録</button>}
     </div>
